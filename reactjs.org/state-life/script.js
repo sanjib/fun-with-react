af8563ffcb53ae071fca7d96657c892ab674965a
Sanjib Ahmad <sanjib@sanjib.org>
2020-05-17 01:15:52 +0800

    Add state and lifecycles Includes clock example.

---
+++ reactjs.org/state-life/script.js
@@ -0,0 +1,68 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Clock = function (_React$Component) {
+    _inherits(Clock, _React$Component);
+
+    function Clock(props) {
+        _classCallCheck(this, Clock);
+
+        var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
+
+        _this.state = { date: new Date() };
+        return _this;
+    }
+
+    _createClass(Clock, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var _this2 = this;
+
+            this.timerID = setInterval(function () {
+                return _this2.setState({ date: new Date() });
+            }, 1000);
+        }
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Tick Tock'
+                ),
+                React.createElement(
+                    'h2',
+                    null,
+                    this.state.date.toLocaleTimeString()
+                )
+            );
+        }
+    }]);
+
+    return Clock;
+}(React.Component);
+
+function App() {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(Clock, null),
+        React.createElement(Clock, null),
+        React.createElement(Clock, null)
+    );
+}
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('area1'));
